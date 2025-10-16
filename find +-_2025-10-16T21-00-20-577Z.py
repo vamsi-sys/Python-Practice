bafde334d2c84b0b05a54abc4f181712289736ee
Vamsikrishna Sirimalla <136159036+vamsi-sys@users.noreply.github.com>
2025-10-17 02:30:20 +0530

    Uploaded via Google Drive + n8n

---
+++ find +-_2025-10-16T21-00-20-577Z.py
@@ -0,0 +1,8 @@
+#check the number is a positive or negative or zero
+e = int(input('enter a number: '))
+if e > 0:
+    print('positive')
+elif e < 0:
+    print('negative')
+else:
+    print('zero')
